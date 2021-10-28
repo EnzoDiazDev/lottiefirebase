a9ba8b1933894e67f3ef1b25f9742a9ca2ca7b29
EnzoDiazDev <enzodiazdev@gmail.com>
2021-10-27 22:00:14 -0300

    secure env

--- src/utils/Env.ts
+++ src/utils/Env.ts
@@ -16,8 +16,14 @@ export default class Env {
     public static get FIREBASE_PRIVATE_KEY():string {
         const private_key_base64 = process.env.FIREBASE_PRIVATE_KEY;
         if (private_key_base64 === undefined) throw new UndefinedEnvVar("FIREBASE_PRIVATE_KEY");
-        if(!private_key_base64.match(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/)) throw new Error("FIREBASE_PRIVATE_KEY is not a valid base64 string");
 
-        return Buffer.from(private_key_base64, "base64").toString("ascii").replace(/\\n/g, "\n");
+        const is_base64 = private_key_base64.match(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/);
+        if(!is_base64) throw new Error("FIREBASE_PRIVATE_KEY is not a valid base64 string");
+
+        try {
+            return Buffer.from(private_key_base64, "base64").toString("ascii").replace(/\\n/g, "\n");
+        } catch {
+            throw new Error("FIREBASE_PRIVATE_KEY is not a valid base64 string");
+        }
     }
 }
